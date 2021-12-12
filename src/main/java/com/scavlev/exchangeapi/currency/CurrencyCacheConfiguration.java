1cbfbc595c392c25733447a331174d3fa8d749aa
scavlev <scavlev@gmail.com>
2021-12-12 18:24:58 +0200

    configurable cache

--- src/main/java/com/scavlev/exchangeapi/currency/CurrencyCacheConfiguration.java
+++ src/main/java/com/scavlev/exchangeapi/currency/CurrencyCacheConfiguration.java
@@ -1,23 +1,26 @@
 package com.scavlev.exchangeapi.currency;
 
 import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.concurrent.TimeUnit;
-
 @EnableCaching
 @Configuration
 class CurrencyCacheConfiguration {
 
+    private final String cacheSpec;
+
+    CurrencyCacheConfiguration(@Value("${app.freecurrencyapi.cache-spec:maximumSize=500, expireAfterWrite=5m}") String cacheSpec) {
+        this.cacheSpec = cacheSpec;
+    }
+
     @Bean
     CacheManager currencyCacheManager() {
-        Caffeine<Object, Object> caffeineCacheBuilder = Caffeine.newBuilder()
-                .maximumSize(500)
-                .expireAfterWrite(5, TimeUnit.MINUTES);
+        Caffeine<Object, Object> caffeineCacheBuilder = Caffeine.from(cacheSpec);
 
         CaffeineCacheManager cacheManager = new CaffeineCacheManager("currencies");
         cacheManager.setCaffeine(caffeineCacheBuilder);
